f8df6a7304789f6b18eddbe356d4809abe0f74b7
umesh madane <umeshmadane2001@gmail.com>
2024-06-17 18:42:13 +0530

    header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css'; // Importing global styles
 import reportWebVitals from './reportWebVitals'; // Importing performance measuring tools
 import { BrowserRouter as Router } from 'react-router-dom'; // Importing Router for client-side routing
 import App from './App'; // Importing the main App component
+import Header from './Components/Header';
 
 // Create the root for rendering the application
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // Render the application with Redux and Router
 root.render(
     <Router>
+    <Header/>
       <div>it is the main change</div>
       <App />
     </Router>
