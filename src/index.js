79603355e2d760858055120e4c1af1fa5c03e22b
umesh madane <umeshmadane2001@gmail.com>
2024-06-17 18:35:13 +0530

    change

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css'; // Importing global styles
 import reportWebVitals from './reportWebVitals'; // Importing performance measuring tools
 import { BrowserRouter as Router } from 'react-router-dom'; // Importing Router for client-side routing
-
-import { Provider } from 'react-redux'; // Importing Redux provider for state management
-import store from './store/store'; // Importing Redux store
 import App from './App'; // Importing the main App component
 
 // Create the root for rendering the application
@@ -14,7 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // Render the application with Redux and Router
 root.render(
     <Router>
-      <App />
+      <div>asdfsfdj</div>
+      {/* <App /> */}
     </Router>
 );
 
