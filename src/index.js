0eae14136b85fe3717724a37664ca000ade97509
umesh madane <umeshmadane2001@gmail.com>
2024-06-18 10:41:31 +0530

    Added button in 404

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css'; // Importing global styles
-import reportWebVitals from './reportWebVitals'; // Importing performance measuring tools
-import { BrowserRouter as Router } from 'react-router-dom'; // Importing Router for client-side routing
-import App from './App'; // Importing the main App component
-import Header from './Components/Header';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles/index.css"
+import reportWebVitals from "./reportWebVitals"
+import { BrowserRouter as Router } from "react-router-dom"
+import App from "./App"
+import Header from "./Components/Header"
 
-// Create the root for rendering the application
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// Render the application with Redux and Router
 root.render(
-    <Router>
-    <Header/>
-      <App />
-    </Router>
-);
+  <Router>
+    <Header />
+    <App />
+  </Router>
+)
 
-// Start measuring performance in the app
-reportWebVitals();
+reportWebVitals()
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,20 @@
+// src/pages/NotFoundPage.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import '../styles/NotFoundPage.css'; // Import CSS file for styling
 
 const NotFoundPage = () => {
   return (
     <div className="not-found-container">
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
+      <div className="links-container">
+        <Link to="/" className="link">Popular</Link>
+        <Link to="/toprated" className="link">Top Rated</Link>
+        <Link to="/upcoming" className="link">Upcoming</Link>
+      <p>👆 &nbsp; Click Above.</p>
+      </div>
     </div>
   );
 };
